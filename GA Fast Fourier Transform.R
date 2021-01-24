e7d617922f6b76010c8131c04d3548d79fae4278
ZHUANG, WEI-HAO <weihaozhaung@gmail.com>
2021-01-24 18:57:26 +0100

    Update and rename Fast Fourier Transform.R to GA Fast Fourier Transform.R

--- GA Fast Fourier Transform.R
+++ GA Fast Fourier Transform.R
@@ -1,17 +1,17 @@
-rm(list=ls());gc()
-setwd("C:/Users/USER/Desktop/MT Project/BigData/BDF20190923")
+rm(list=ls());gc() # clean momery
+setwd("C:/Users/USER/Desktop/MT Project/BigData/BDF20190923") #set working directionary
 library(tidyverse)
 library(ggplot2)
 library(data.table)
 library(imager)
 library(spectral)
 
-ga=fread(file="Analytics All Web Site Data 目標對象總覽 20180130-20190922.csv",data.table = F)
+ga=fread(file="ga.csv",data.table = F)
 colnames(ga)=c("date","hits") 
 ga$date<-as.Date(ga$date)
 ga$hits <-ga$hits %>% as.numeric() 
 ga<-ga%>% na.omit()
-ga<-ga %>% mutate(wk=week(date),yr=year(date)) %>%
+ga<-ga %>% mutate(wk=week(date),yr=year(date)) %>% # aggregating weekly hits
   mutate(yr_wk=paste0(as.character(yr),'-',as.character(wk))) %>%
   group_by(yr_wk) %>% summarise(wkly_hits=sum(hits))
 
@@ -22,12 +22,14 @@ ga<-ga %>% mutate(wk=week(date),yr=year(date)) %>%
 
 y=ga$wkly_hits
 x=1:nrow(ga)
- FT <- spec.fft(y, x)
+ FT <- spec.fft(y, x) #fft function
+
+#draw plot
   par(mfrow = c(1, 1))
   plot(x, y, type = "l", main = "Signal")
   abline(v=26*3)
   
-  abline(v=16.12903*1+10)
+  abline(v=16.12903*1+10) 
   abline(v=0.062)
   abline(v=0.145)
 plot(
